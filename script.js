96118d04cfd972362ce1ef2856c3610f4626e344
ACHUTHAN <81421360+angstromachu@users.noreply.github.com>
2021-05-21 17:13:44 +0530

    Project files added

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// for navbar 
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("nav").style.top = "0";
+    } else {
+        document.getElementById("nav").style.top = "-10rem";
+    }
+}
+
+// for hamburger animation
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
